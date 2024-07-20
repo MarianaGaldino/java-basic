f309e758f7158991df378a8b5b733ab549855415
Mariana Galdino <80767881+MarianaGaldino@users.noreply.github.com>
2024-07-20 13:02:28 -0300

    Finalizando primeira aula

---
+++ Primeiro.java
@@ -0,0 +1,15 @@
+//classe em Java - comentario em linha
+public class Primeiro{
+/*
+	** comentario em Bloco
+	1 - arquivos em java devem ser criados como o mesmo nome das classes
+	2 - O nome do arquivo deve sempre iniciar em Maiúsculo. Sistema PascalCase
+	3 - PascalCase: primeira palavra sempre maiúscula: projetos e nomes de classes
+	camelCase: inicia com minúscula: atributos e funcoes
+*/
+
+	public static void main (String args[]){
+	// metodo publico estatico que nao tem retorno
+		System.out.println("Olá mundo!");
+	}	
+}
